1c7382ab71c549c80d3616add307541ac799d9a5
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2022-03-09 09:43:34 +0000

    Updated camera from engine development

--- FluidDynamics/Core/Components/Camera/Camera.cpp
+++ FluidDynamics/Core/Components/Camera/Camera.cpp
@@ -3,11 +3,14 @@
 #include "Utility/Input System/InputSystem.h"
 #include "Utility/Math/Math.h"
 
-Camera::Camera() : projectionMatrix(), viewMatrix(), rotation(), 
-				   defaultForward(DirectX::XMFLOAT3(0, 0, 1)), defaultRight(DirectX::XMFLOAT3(1, 0, 0)), defaultUp(DirectX::XMFLOAT3(0, 1, 0)),
-				   forward(), right(), up(), target(), transform(), position()
+Camera::Camera() : speed(1), rotation(), forward(), right(), up(), target(), projectionMatrix(), viewMatrix(), transform(nullptr), position(nullptr)
 {
+	defaultForward = DirectX::XMFLOAT3(0, 0, 1);
+	defaultRight = DirectX::XMFLOAT3(1, 0, 0);
+	defaultUp = DirectX::XMFLOAT3(0, 1, 0);
+
 	DirectX::XMFLOAT3 tempPosition = DirectX::XMFLOAT3(0, 0, 0);
+
 	XMStoreFloat4x4(&viewMatrix, DirectX::XMMatrixLookAtLH(XMLoadFloat3(&tempPosition), XMLoadFloat3(&defaultForward), XMLoadFloat3(&defaultUp)));
 
 	this->setType(ComponentTypes::Camera);
@@ -27,7 +30,7 @@ void Camera::setTransfrom(Transform* trans)
 void Camera::updateProjection(int width, int height)
 {
 	//Only need to change this if we are resizing the window.
-	XMStoreFloat4x4(&projectionMatrix,XMMatrixPerspectiveFovLH(DirectX::XM_PIDIV2, width / (FLOAT)height, 0.01f, 100.0f));
+	XMStoreFloat4x4(&projectionMatrix,XMMatrixPerspectiveFovLH(DirectX::XM_PIDIV2, width / (FLOAT)height, 0.01f, 1000.0f));
 }
 
 void Camera::Update(float deltaTime)
@@ -58,26 +61,32 @@ void Camera::Update(float deltaTime)
 	DirectX::XMVECTOR posVec = XMLoadFloat3(position);
 
 	// Movement.
-	int speed = 10;
-
-	if (GetAsyncKeyState(VK_UP))
+	if (GetAsyncKeyState(0x57)) // W
 	{
 		posVec += (speed * deltaTime) * XMLoadFloat3(&forward);
 	}
 
-	if (GetAsyncKeyState(VK_DOWN))
+	if (GetAsyncKeyState(0x53)) // S
 	{
 		posVec -= (speed * deltaTime) * XMLoadFloat3(&forward);
 	}
 
-	if (GetAsyncKeyState(VK_LEFT))
+	if (GetAsyncKeyState(0x41)) // A
 	{
 		posVec -= (speed * deltaTime) * XMLoadFloat3(&right);
 	}
 
-	if (GetAsyncKeyState(VK_RIGHT))
+	if (GetAsyncKeyState(0x44)) // D
 	{
 		posVec += (speed * deltaTime) * XMLoadFloat3(&right);
+	}
+
+	if(GetAsyncKeyState(VK_LSHIFT))
+	{
+		speed = initialSpeed * boost;
+	}else
+	{
+		speed = initialSpeed;
 	}
 
 	XMStoreFloat4x4(&viewMatrix,XMMatrixLookToLH(posVec, XMLoadFloat3(&target), XMLoadFloat3(&up)));
--- FluidDynamics/Core/Components/Camera/Camera.h
+++ FluidDynamics/Core/Components/Camera/Camera.h
@@ -20,8 +20,6 @@ public:
 	void setPitch(float value) { pitch = value; };
 
 private:
-	DirectX::XMFLOAT4X4 projectionMatrix;
-	DirectX::XMFLOAT4X4 viewMatrix;
 
 	float rightMag = 0;
 	float leftMag = 0;
@@ -30,6 +28,10 @@ private:
 
 	float pitch = 0;
 	float yaw = 0;
+
+	int initialSpeed = 10;
+	int speed;
+	int boost = 5;
 
 	DirectX::XMFLOAT4X4 rotation;
 
@@ -42,6 +44,8 @@ private:
 	DirectX::XMFLOAT3 up;
 	DirectX::XMFLOAT3 target;
 
+	DirectX::XMFLOAT4X4 projectionMatrix;
+	DirectX::XMFLOAT4X4 viewMatrix;
 
 	Transform* transform;
 	DirectX::XMFLOAT3* position;
