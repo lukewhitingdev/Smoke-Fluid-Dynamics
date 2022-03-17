f75bf6590dd71306b65a84f24bf992eb66804975
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2022-03-17 16:09:12 +0000

    Update Math.h

--- FluidDynamics/Utility/Math/Math.h
+++ FluidDynamics/Utility/Math/Math.h
@@ -9,5 +9,15 @@ public:
 	{
 		return std::max<float>(min, std::min<float>(val, max));
 	}
+
+	static int clamp(const int val, const int min, int max)
+	{
+		return std::max<int>(min, std::min<int>(val, max));
+	}
+
+	static int random(int min,int max)
+	{
+		return min + (rand() % static_cast<int>(max - min + 1));
+	}
 };
 
