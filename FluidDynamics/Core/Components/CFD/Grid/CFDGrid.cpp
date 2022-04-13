8499c7b3570740ecfc2e71efb86788713ed86f49
LukeWhitingDev <lukewhitingworkemail@gmail.com>
2022-04-13 16:08:55 +0100

    Update CFDGrid.cpp

--- FluidDynamics/Core/Components/CFD/Grid/CFDGrid.cpp
+++ FluidDynamics/Core/Components/CFD/Grid/CFDGrid.cpp
@@ -10,45 +10,6 @@ CFDGrid::~CFDGrid()
 {
 }
 
-void CFDGrid::Start()
-{
-	D3D* direct3D = D3D::getInstance();
-
-	D3D11_TEXTURE3D_DESC texDesc = {};
-	ZeroMemory(&texDesc, sizeof(D3D11_TEXTURE3D_DESC));
-	texDesc.Width = width;
-	texDesc.Height = height;
-	texDesc.Depth = depth;
-	texDesc.MipLevels = 1;
-	texDesc.Format = DXGI_FORMAT_R32_FLOAT;
-	texDesc.Usage = D3D11_USAGE_DEFAULT;
-	texDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
-
-	if (FAILED(direct3D->device->CreateTexture3D(&texDesc, nullptr, &voxelTex)))
-		throw;
-
-	D3D11_SHADER_RESOURCE_VIEW_DESC texViewDesc = {};
-	ZeroMemory(&texViewDesc, sizeof(D3D11_SHADER_RESOURCE_VIEW_DESC));
-	texViewDesc.Format = texDesc.Format;
-	texViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE3D;
-	texViewDesc.Texture2D.MipLevels = texDesc.MipLevels;
-	texViewDesc.Texture2D.MostDetailedMip = 0;
-
-	direct3D->device->CreateShaderResourceView(voxelTex, &texViewDesc, &voxelView);
-
-	// Create Sampler
-	D3D11_SAMPLER_DESC sampDesc;
-	ZeroMemory(&sampDesc, sizeof(sampDesc));
-	sampDesc.Filter = D3D11_FILTER_ANISOTROPIC;
-	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
-	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
-	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
-	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
-	sampDesc.MinLOD = 0;
-	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
-	direct3D->device->CreateSamplerState(&sampDesc, &sampler);
-}
-
 void CFDGrid::setGrid(long long w, long long h, long long d)
 {
 	width = w;
@@ -102,44 +63,6 @@ CFDVoxel* CFD::CFDGrid::getVoxelPreviousFrame(int x, int y, int z)
 	return getVoxel(x, y, z, voxels0);
 }
 
-void CFDGrid::Update(float deltaTime)
-{
-	static int iter;
-	D3D* direct3D = D3D::getInstance();
-
-	//printf("Post Source: \n");
-	//this->printGridInfomation(voxels);
-
-	//system("cls");
-
-	updateDiffuse(0.016f);
-	updatePreviousDiffuse();
-
-	//printf("\n Post Diffusion: \n");
-	//this->printGridInfomation(voxels);
-
-	//direct3D->immediateContext->UpdateSubresource(voxelTex, 0, nullptr, voxels, width, depth);
-	//printf("Iteration: %d \n", iter);
-
-	// Test, this previous density needs to be equal if its using a stable method since we can backtrace linear methods.
-	//printf("Density[1,1,1] Prev: %f \n", this->getDensityPreviousFrame(0, 0, 0));
-	//printf("Density[1,1,1] Curr: %f \n", this->getDensity(0, 0, 0));
-
-	//printf("%f \n", this->getVoxel(0, 0, 0, voxels)->data->density);
-	//printf("%f \n", this->getVoxel(0, 0, 0, voxels0)->data->density);
-
-	iter++;
-}
-
-void CFDGrid::Render()
-{
-	D3D* direct3D = D3D::getInstance();
-
-	direct3D->immediateContext->UpdateSubresource(voxelTex, 0, nullptr, voxels, width, depth);
-	direct3D->immediateContext->PSSetSamplers(0, 1, &sampler);
-	direct3D->immediateContext->PSSetShaderResources(0, 1, &voxelView);
-}
-
 void CFD::CFDGrid::addDensitySource(int x, int y, int z, float density)
 {
 	if (x > width || y > height || z > depth)
@@ -160,39 +83,6 @@ float CFD::CFDGrid::getDensity(int x, int y, int z)
 	{
 		return -1.0f;
 	}
-}
-
-float CFD::CFDGrid::getDensityPreviousFrame(int x, int y, int z)
-{
-	CFDVoxel* center = this->getVoxel(x, y, z, voxels);
-
-	CFDVoxel* left = this->getVoxel(x - 1, y, z, voxels);
-	float leftDens = (left) ? left->data->density : 0;
-
-	CFDVoxel* right = this->getVoxel(x + 1, y, z, voxels);
-	float rightDens = (right) ? right->data->density : 0;
-
-	CFDVoxel* up = this->getVoxel(x, y + 1, z, voxels);
-	float upDens = (up) ? up->data->density : 0;
-
-	CFDVoxel* down = this->getVoxel(x, y - 1, z, voxels);
-	float downDens = (down) ? down->data->density : 0;
-
-	CFDVoxel* front = this->getVoxel(x, y, z + 1, voxels);
-	float forwardDens = (front) ? front->data->density : 0;
-
-	CFDVoxel* back = this->getVoxel(x, y, z - 1, voxels);
-	float backDens = (back) ? back->data->density : 0;
-
-	// Calculate the diffuse from our neighbours.
-	float xDiffuse = leftDens + rightDens;
-	float yDiffuse = upDens + downDens;
-	float zDiffuse = forwardDens + backDens;
-
-	// Add them up.
-	float totalDiffuse = xDiffuse + yDiffuse + zDiffuse;
-
-	return center->data->density - diffuse * totalDiffuse - 4 * center->data->density;
 }
 
 void CFD::CFDGrid::updateDiffuse(float deltaTime)
@@ -272,7 +162,89 @@ void CFD::CFDGrid::updateDiffuse(float deltaTime)
 	}
 }
 
-void CFD::CFDGrid::updatePreviousDiffuse()
+void CFD::CFDGrid::updateAdvection(float deltaTime)
+{
+}
+
+void CFD::CFDGrid::updatePreviousPreviousFrameVoxels()
 {
 	memcpy_s(voxels0, sizeof(CFDVoxel) * totalVoxels, voxels, sizeof(CFDVoxel) * totalVoxels);
+}
+
+void CFDGrid::Start()
+{
+	D3D* direct3D = D3D::getInstance();
+
+	D3D11_TEXTURE3D_DESC texDesc = {};
+	ZeroMemory(&texDesc, sizeof(D3D11_TEXTURE3D_DESC));
+	texDesc.Width = width;
+	texDesc.Height = height;
+	texDesc.Depth = depth;
+	texDesc.MipLevels = 1;
+	texDesc.Format = DXGI_FORMAT_R32_FLOAT;
+	texDesc.Usage = D3D11_USAGE_DEFAULT;
+	texDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+
+	if (FAILED(direct3D->device->CreateTexture3D(&texDesc, nullptr, &voxelTex)))
+		throw;
+
+	D3D11_SHADER_RESOURCE_VIEW_DESC texViewDesc = {};
+	ZeroMemory(&texViewDesc, sizeof(D3D11_SHADER_RESOURCE_VIEW_DESC));
+	texViewDesc.Format = texDesc.Format;
+	texViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE3D;
+	texViewDesc.Texture2D.MipLevels = texDesc.MipLevels;
+	texViewDesc.Texture2D.MostDetailedMip = 0;
+
+	direct3D->device->CreateShaderResourceView(voxelTex, &texViewDesc, &voxelView);
+
+	// Create Sampler
+	D3D11_SAMPLER_DESC sampDesc;
+	ZeroMemory(&sampDesc, sizeof(sampDesc));
+	sampDesc.Filter = D3D11_FILTER_ANISOTROPIC;
+	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	sampDesc.MinLOD = 0;
+	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
+	direct3D->device->CreateSamplerState(&sampDesc, &sampler);
+}
+
+void CFDGrid::Update(float deltaTime)
+{
+	static int iter;
+	D3D* direct3D = D3D::getInstance();
+
+	//printf("Post Source: \n");
+	//this->printGridInfomation(voxels);
+
+	//system("cls");
+
+	updateDiffuse(0.016f);
+
+	//printf("\n Post Diffusion: \n");
+	//this->printGridInfomation(voxels);
+
+	//direct3D->immediateContext->UpdateSubresource(voxelTex, 0, nullptr, voxels, width, depth);
+	//printf("Iteration: %d \n", iter);
+
+	// Test, this previous density needs to be equal if its using a stable method since we can backtrace linear methods.
+	//printf("Density[1,1,1] Prev: %f \n", this->getDensityPreviousFrame(0, 0, 0));
+	//printf("Density[1,1,1] Curr: %f \n", this->getDensity(0, 0, 0));
+
+	//printf("%f \n", this->getVoxel(0, 0, 0, voxels)->data->density);
+	//printf("%f \n", this->getVoxel(0, 0, 0, voxels0)->data->density);
+
+	iter++;
+
+	updatePreviousPreviousFrameVoxels();
+}
+
+void CFDGrid::Render()
+{
+	D3D* direct3D = D3D::getInstance();
+
+	direct3D->immediateContext->UpdateSubresource(voxelTex, 0, nullptr, voxels, width, depth);
+	direct3D->immediateContext->PSSetSamplers(0, 1, &sampler);
+	direct3D->immediateContext->PSSetShaderResources(0, 1, &voxelView);
 }
